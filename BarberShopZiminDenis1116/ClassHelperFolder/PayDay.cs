770e882c9afeaadae8ffbfb41c2936c66e3bf0b4
Антон Федяев <fearthe54@gmail.com>
2022-03-15 12:12:54 +0300

    Otchet

---
+++ BarberShopZiminDenis1116/ClassHelperFolder/PayDay.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberShopZiminDenis1116.ClassHelperFolder
+{
+    class PayDay
+    {
+        
+    }
+}
--- BarberShopZiminDenis1116/EFDataBaseFolder/Personal.cs
+++ BarberShopZiminDenis1116/EFDataBaseFolder/Personal.cs
@@ -32,6 +32,8 @@ namespace BarberShopZiminDenis1116.EFDataBaseFolder
         public string PersonalPassword { get; set; }
         public Nullable<bool> IsDeleted { get; set; }
         public byte[] PersonalPhoto { get; set; }
+        public Nullable<decimal> Pay { get; set; }
+        public Nullable<double> Coeff { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Notes> Notes { get; set; }
